3aee9ca1fcb98375c4400da2bcb41867708f6e3b
Adam <AdamBCo@me.com>
2016-06-09 06:31:53 -0700

    Created List

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,9 +1,35 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var ToDoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the Dog'
+        },
+        {
+          id: 2,
+          text: 'Clean the House'
+        },
+        {
+          id: 3,
+          text: 'Take out the trash'
+        },
+        {
+          id: 4,
+          text: 'Wash the Windows'
+        }
+      ]
+    }
+  },
   render: function () {
+
+    var {todos} = this.state;
+
     return (
-      <div>ToDoApp.jsx</div>
+      <TodoList todos={todos}/>
     )
   }
 });
