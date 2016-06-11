4f1add476d1174502613698b68639e3919d591ce
Adam <AdamBCo@me.com>
2016-06-10 22:23:51 -0700

    Add filter tests

--- app/tests/components/TodoAPI.test.jsx
+++ app/tests/components/TodoAPI.test.jsx
@@ -82,17 +82,24 @@ describe('TodoAPI', () => {
       var filteredTodos = TodoAPI.filterTodos(todos, false, '');
       expect(filteredTodos.length).toBe(2);
     });
-    //
-    // it('should return todos if valid array in localStorage', () => {
-    //   var todos = [{
-    //     id: 11,
-    //     text: 'Hello Todo',
-    //     completed: false
-    //   }];
-    //   localStorage.setItem('todos', JSON.stringify(todos));
-    //   var actualTodos = TodoAPI.getTodos();
-    //   expect(actualTodos).toEqual(todos);
-    // });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by searchText', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'bad');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should filter return all todos if searchText is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+
+
   });
 
 });
