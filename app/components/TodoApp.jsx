b0b4a1dcf239c9451025892bda5984a2f89096b6
Adam <AdamBCo@me.com>
2016-06-10 21:31:09 -0700

    Added localStorage

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,39 +1,21 @@
 var React = require('react');
+var uuid = require('node-uuid');
+
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
-var uuid = require('node-uuid');
+var TodoAPI = require('TodoAPI');
 
 var ToDoApp = React.createClass({
   getInitialState: function () {
     return {
       showCompleted: false,
       searchText: '',
-      todos: [
-        {
-          id: uuid(),
-          text: 'Walk the Dog',
-          completed: true
-        },
-        {
-          id: uuid(),
-          text: 'Clean the House',
-          completed: true
-
-        },
-        {
-          id: uuid(),
-          text: 'Take out the trash',
-          completed: false
-
-        },
-        {
-          id: uuid(),
-          text: 'Wash the Windows',
-          completed: false
-        }
-      ]
+      todos: TodoAPI.getTodos()
     }
+  },
+  componentDidUpdate: function () {
+    TodoAPI.setTodos(this.state.todos);
   },
   handleAddToDo: function (text) {
     this.setState({
