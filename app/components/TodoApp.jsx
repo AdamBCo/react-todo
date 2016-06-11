7f583e86a644c41717dc97f251e702280389e255
Adam <AdamBCo@me.com>
2016-06-10 21:01:28 -0700

    Added completed check box

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -12,19 +12,25 @@ var ToDoApp = React.createClass({
       todos: [
         {
           id: uuid(),
-          text: 'Walk the Dog'
+          text: 'Walk the Dog',
+          completed: true
         },
         {
           id: uuid(),
-          text: 'Clean the House'
+          text: 'Clean the House',
+          completed: true
+
         },
         {
           id: uuid(),
-          text: 'Take out the trash'
+          text: 'Take out the trash',
+          completed: false
+
         },
         {
           id: uuid(),
-          text: 'Wash the Windows'
+          text: 'Wash the Windows',
+          completed: false
         }
       ]
     }
@@ -35,7 +41,8 @@ var ToDoApp = React.createClass({
         ...this.state.todos,
         {
           id: uuid(),
-          text: text
+          text: text,
+          completed: false
         }
       ]
     })
@@ -46,6 +53,16 @@ var ToDoApp = React.createClass({
       searchText: searchText.toLowerCase()
     })
   },
+  handleToggle: function(id) {
+    var updatedTodos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+
+    this.setState({todos:updatedTodos});
+  },
   render: function () {
 
     var {todos} = this.state;
@@ -54,7 +71,7 @@ var ToDoApp = React.createClass({
       <div className="columns medium-6 large-4 small-centered">
         <h1>Todo App</h1>
         <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={todos}/>
+        <TodoList todos={todos} onToggle={this.handleToggle}/>
         <AddTodo onAddTodo={this.handleAddToDo}/>
       </div>
     )
