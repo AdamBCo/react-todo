976ff17839ea597f21d3a76c41b9ae538573154d
Adam <AdamBCo@me.com>
2016-06-22 07:02:28 -0700

    Added async test for action

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -1,5 +1,10 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 var expect = require('expect');
+
 var actions = require('actions');
+
+var createMockStore = configureMockStore([thunk]);
 
 describe('Actions', () => {
   it('should generate search text action', () => {
@@ -35,6 +40,24 @@ describe('Actions', () => {
 
     expect(res).toEqual(action);
   });
+
+  it('should create todo and dispatch add todo ADD_TODO', (done) => {
+    const store = createMockStore({});
+    const todoText = 'My todo item';
+
+    store.dispatch(actions.startAddTodo(todoText)).then(()=> {
+        const actions = store.getActions();
+        expect(actions[0]).toInclude({
+          type: 'ADD_TODO'
+        });
+        expect(actions[0].todo).toInclude({
+          text: todoText
+        });
+        done();
+    }).catch(done);
+
+  });
+
 
   it('should generate add todos action object', () => {
     var todos = [{
